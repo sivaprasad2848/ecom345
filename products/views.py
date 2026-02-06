3cade0ab9cf8f070be56a8ed59d8c1f1b8c02e60
sivaprasad2848 <sivaprasad@cybersquare.org>
2026-02-06 15:09:10 +0530

    Products File Added

--- products/views.py
+++ products/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Products
 from category.models import Category
+
 # Create your views here.
 def product_list(request):
     cat=Category.objects.all()
-    return render(request,"products.html",{'category':cat})
+    products=Products.objects.all()
+    return render(request,"products.html",{'category':cat,'products':products})
 def product_create(request):
-    return HttpResponse("ProductCreate")
+    if request.method=="POST":
+        name=request.POST['txtPname']
+        category_id=request.POST['selCategory']
+        price=request.POST['txtUnitprice']
+        stock=request.POST['txtStock']
+        desc=request.POST['txtDesc']
+        image="test"
+        category = Category.objects.get(id=category_id)
+        p=Products(name=name,catid=category,price=price,stock=stock,description=desc,images=image)
+        p.save()
+    return redirect('product_list')
 def product_delete(request,id):
     return HttpResponse("ProductDelete")
 def product_update(request):
